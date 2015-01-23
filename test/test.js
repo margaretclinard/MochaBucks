0f963fa8bc3128c9694290375105cda4f0b1af58
Margaret Clinard <margaret.clinard@gmail.com>
2015-01-23 13:31:33 -0600

    added .jscsrc .jshintrc and test.js  and modified package.json

---
+++ test/test.js
@@ -0,0 +1,14 @@
+/* global describe, it */
+/* jshint expr: true */
+
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('Mocha', function () {
+  it('should expect true and false', function () {
+    expect(true).to.be.true;
+    expect(false).to.be.false;
+  });
+});
+
